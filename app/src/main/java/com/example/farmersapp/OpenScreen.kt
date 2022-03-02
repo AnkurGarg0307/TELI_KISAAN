85463bc0534f125de832a5001b3f662fdf798610
ANKUR GARG <gargankur1116@gmail.com>
2022-03-02 17:06:09 +0530

    INITIAL COMMIT

---
+++ app/src/main/java/com/example/farmersapp/OpenScreen.kt
@@ -0,0 +1,28 @@
+package com.example.farmersapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.farmersapp.databinding.ActivitySplashBinding
+
+class OpenScreen : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler: Boolean = Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java
+                )
+            )
+            finish()
+        }, 5000)
+
+
+    }
+}
