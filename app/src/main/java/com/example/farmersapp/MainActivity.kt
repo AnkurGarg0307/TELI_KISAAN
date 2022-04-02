4ddd5a666413370e975a1e4ad46eff2de68d92dd
singhal-ekansh <ekansin05@gmail.com>
2022-04-02 13:47:40 +0530

    dashboard initial commit

--- app/src/main/java/com/example/farmersapp/MainActivity.kt
+++ app/src/main/java/com/example/farmersapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.farmersapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.farmersapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.schemaCard.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this,GovernmentSchemas::class.java))
+        })
+
     }
 }
