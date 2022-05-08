cfbc10de9ab6213a27ece34d6c6ebe25a3fccf0b
Akshat-gour <Akshatgour17@gmail.com>
2022-05-09 00:21:57 +0530

    added List View For Govt Scheme and About Crop

---
+++ app/src/main/java/com/example/farmersapp/adapters/AboutCropAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.farmersapp.adapters
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.farmersapp.Description_Activity
+import com.example.farmersapp.R
+import com.example.farmersapp.dashboard.AboutCrop
+import com.example.farmersapp.models.CropItem
+
+class AboutCropAdapter(private val mList: List<CropItem>): RecyclerView.Adapter<AboutCropAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.crop_card, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val ItemsViewModel = mList[position]
+
+        holder.imageView.setImageResource(ItemsViewModel.image)
+
+        holder.textView.setText(ItemsViewModel.text)
+        holder.item.setOnClickListener(View.OnClickListener {V->
+            V.context.startActivity(Intent(V.context, Description_Activity::class.java))
+        })
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView2)
+        val textView: TextView = itemView.findViewById(R.id.textView5)
+        val item: LinearLayout = itemView.findViewById(R.id.cropItem)
+    }
+}
---
+++ app/src/main/java/com/example/farmersapp/models/SchemeItem.kt
@@ -0,0 +1,5 @@
+package com.example.farmersapp.models
+
+data class SchemeItem(val image: Int, val text: String, val description: String) {
+
+}
