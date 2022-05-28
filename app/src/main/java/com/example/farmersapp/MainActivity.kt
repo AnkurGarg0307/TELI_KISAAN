8eb0ab3d6a4a0785869065d6e0ff799f3a64ec97
singhal-ekansh <ekansin05@gmail.com>
2022-05-29 02:58:23 +0530

    weatherInfo

--- app/src/main/java/com/example/farmersapp/MainActivity.kt
+++ app/src/main/java/com/example/farmersapp/MainActivity.kt
@@ -9,7 +9,7 @@ import com.example.farmersapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -30,6 +30,9 @@ class MainActivity : AppCompatActivity() {
         }
         binding.moistureCard.setOnClickListener {
             startActivity(Intent(this, SoilMoisture::class.java))
+        }
+        binding.weatherCard.setOnClickListener {
+            startActivity(Intent(this, WeatherInfo::class.java))
         }
     }
 }
